d2ad7d5c083b891b61df9e4d519249a6e750f5db
Proloycb <proloychacroborty@gmail.com>
2022-05-08 18:09:54 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,31 @@ const port = process.env.PORT || 5000;
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vm2fc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        const inventoryCollection = client.db('gymEquipment').collection('inventory');
+        
+        // inventory api
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+    }
+    finally{}
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Running gym equipment warehouse');
