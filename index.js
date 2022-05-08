3424a5f247e46282098627cd1b814237d703c295
Proloycb <proloychacroborty@gmail.com>
2022-05-08 15:14:39 +0600

    server implement

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Running gym equipment warehouse');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+});
